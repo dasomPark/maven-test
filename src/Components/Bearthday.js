aff70b833c24a314ef4ca51536a6286ac945061b
dasomPark <dpark@juliusworks.com>
2021-01-08 14:40:50 -0500

    Added axios to fetch images from NASA EPIC API.

--- src/Components/Bearthday.js
+++ src/Components/Bearthday.js
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
-import './Bearthday.css';
+import React, { useState, useEffect } from 'react';
 import { DropdownDate } from 'react-dropdown-date';
+import axios from 'axios';
+import './Bearthday.css';
 
 const Bearthday = () => {
     const [date, setDate] = useState(new Date(2020, 3, 22));
+    const [iamges, setImages] = useState([]);
+    const getImageUrl = (fileName) => {
+        return `https://epic.gsfc.nasa.gov/archive/enhanced/${formatDate(date, '/')}/png/${fileName}.png`;
+    }
+
+    useEffect(() => {
+        axios.get(`https://epic.gsfc.nasa.gov/api/enhanced/date/${formatDate(date)}`)
+            .then(({data}) => {
+                setImages(data.map(d => d.image));
+            })
+            .catch(err => {
+                console.err(err);
+            })
+    }, [date]);
 
     return (
         <div className="bearthdayContainer">
@@ -29,8 +44,29 @@ const Bearthday = () => {
                 />
                 <button className="bearthdayButton">Submit</button>
             </div>
+            <div className="bearthdayImagesContainer">
+                {
+                    iamges.map((image, idx) => {
+                        return (
+                            <img key={idx} src={getImageUrl(image)} width="200px" height="auto"/>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
+}
+
+const formatDate = (date, seperator = '-') => {
+    let newDate = new Date(date);
+    let month = '' + (newDate.getMonth() + 1);
+    let day = '' + newDate.getDate();
+    let year = newDate.getFullYear();
+
+    if(month.length < 2) month = '0' + month;
+    if(day.length < 2) day = '0' + day;
+
+    return [year, month, day].join(seperator)
 }
 
 export default Bearthday;
